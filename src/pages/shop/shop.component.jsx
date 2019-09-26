72675a3831da09623c0062f924964ee80152623c
baconYao <bacon735392@gmail.com>
2019-09-27 00:43:38 +0800

    Init shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+class Shop extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render() {
+    return <div>SHOP PAGE</div>
+  }
+}
+
+export default Shop;
