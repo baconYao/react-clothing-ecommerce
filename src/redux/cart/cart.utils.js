ee6917984e4a76a419c464eec19c6e1aa852ae3a
baconYao <bacon735392@gmail.com>
2019-11-07 23:42:14 +0800

    update shop data with new data, update cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,22 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  /* 處理新增物品到購物車時，redux 的操作
+   * arguments: 
+   *    cartItems: 已存在 redux cart store 裡面的物品
+   *    cartItemToAdd: 將要被加入 cart store 的單一 item
+   * 
+   * return: 合併後的 cartItems array
+   */
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+
+  // 已經有相同的選項，所以要合併，用 quantity 來計數
+  if(existingCartItem) {
+    return cartItems.map(cartItem => 
+      cartItem.id === cartItemToAdd.id ?
+        { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // 新選項進入時的操作
+  return [...cartItems, { ...cartItemToAdd, quantity: 1}]
+}
