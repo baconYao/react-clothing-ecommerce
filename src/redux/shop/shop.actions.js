cd77cfe7537f390ef3846438e3f41d17f8fb0816
baconYao <bacon735392@gmail.com>
2019-12-15 09:30:03 +0800

    added redux-thunk and refactor shop page component

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,36 @@
 import { shopActionTypes } from './shop.types';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-export const updateCollections = (collectionsMap) => ({
-  type: shopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+export const fetchCollectionsError = (errorMessage) => ({
+  type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+  /*
+  * If redux-thunk middleware is enabled, any time you attempt to dispatch a function instead of 
+  * an object (上面的就是 dispatch object 例子), the middleware will call that function with dispatch method itself as the 
+  * first argument.
+  */
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+    
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap =  convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));      
+      })
+      .catch(error => dispatch(fetchCollectionsError(error)));
+  }
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-var middlewares = [];
+var middlewares = [thunk];
 
+// 上傳到 heroku 就會自動變成 production，因此 logger 的訊息不會顯示在 browser's console
 if(process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
